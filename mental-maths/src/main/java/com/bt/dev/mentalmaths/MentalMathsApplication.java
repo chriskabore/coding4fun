54bde45e6606b2593eac5257a6e89974d3707acf
Christian Kabore <chriskabor@gmail.com>
2022-04-15 13:31:02 +0000

    first commit

---
+++ mental-maths/src/main/java/com/bt/dev/mentalmaths/MentalMathsApplication.java
@@ -0,0 +1,13 @@
+package com.bt.dev.mentalmaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalMathsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalMathsApplication.class, args);
+    }
+
+}
---
+++ mental-maths/src/test/java/com/bt/dev/mentalmaths/MentalMathsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.dev.mentalmaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalMathsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
